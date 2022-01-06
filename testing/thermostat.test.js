64b10ab8809ad667a8de4649dfbddad984d847c6
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-06 14:12:06 +0000

    turnPowerSaving method created to turn off or on powerSaving mode

--- testing/thermostat.test.js
+++ testing/thermostat.test.js
@@ -13,14 +13,25 @@ describe('class Thermostat', () => {
   });
   it('decreases temperature by 1 when down is called', () => {
     thermostat.down()
-    expect(thermostat.getTemperature()).toBe(19)
+    expect(thermostat.getTemperature()).toBe(19);
   });
   it('has a minimum temperature setting', () => {
     for(let i = 0; i < thermostat.minTemperature; i++) {
       thermostat.down();
     }
-    thermostat.down()
-    expect(thermostat.getTemperature()).toBe(10)
+    thermostat.down();
+    expect(thermostat.getTemperature()).toBe(10);
   });
-  
+  it('Power saving mode is on by default', () => {
+    expect(thermostat.powerSaving).toBe(true);
+  });
+  it('Power saving mode can be turned off', () => {
+    thermostat.turnPowerSaving('off')
+    expect(thermostat.powerSaving).toBe(false)
+  });
+  it('Power saving mode can be turned on after being turned off', () => {
+    thermostat.turnPowerSaving('off')
+    thermostat.turnPowerSaving('on')
+    expect(thermostat.powerSaving).toBe(true)
+  });
 });
--- thermostat.js
+++ thermostat.js
@@ -2,6 +2,7 @@ class Thermostat {
   constructor() {
     this.temperature = 20;
     this.minTemperature = 10;
+    this.powerSaving = true;
   };
   getTemperature() {
     return this.temperature;
@@ -12,6 +13,14 @@ class Thermostat {
   down() {
     if(this.temperature > this.minTemperature) {
       this.temperature -= 1;
+    };
+  };
+  turnPowerSaving(choice) {
+    if(choice === 'off') {
+      this.powerSaving = false;
+    }
+    else if(choice === 'on') {
+      this.powerSaving = true;
     };
   };
 };
